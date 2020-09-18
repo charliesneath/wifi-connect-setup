1186508c6fe9a2ec22efe19ee1ff7b15bb51cd2f
Charlie Sneath <charlie.sneath@gmail.com>
2020-09-18 08:33:12 -0400

    comments and changing password

--- start-wifi-connect.sh
+++ start-wifi-connect.sh
@@ -8,14 +8,16 @@ echo "Waiting for network connection for 5s."
 NETWORK_STATUS=$?
 echo "nm-online exit code: ${NETWORK_STATUS}"
 
+# If NetworkManager is not connected, then run wifi-connect
 if (( $NETWORK_STATUS > 0 ))
 then
   echo "No internet connection."
   echo "Starting wifi-connect to create wifi access point..."
-  cd /home/pi/wifi-connect-setup
+  cd /home/pi/wifi-connect-setup/
+  # Start a password-protected wifi access point
   sudo wifi-connect \
        --portal-ssid "rpi-admin" \
-       --portal-passphrase "meteorology"
+       --portal-passphrase "wificonnect"
 fi
 
 echo "Internet connection established"
